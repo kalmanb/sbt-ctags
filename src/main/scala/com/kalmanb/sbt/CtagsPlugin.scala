31274d3509aa2ec197b89c52829dd332c83997a4
kalmanb <kalman@movio.co>
2013-11-03 22:27:28 +1300

    Can now unzip source and update ctags

--- src/main/scala/com/kalmanb/sbt/CtagsPlugin.scala
+++ src/main/scala/com/kalmanb/sbt/CtagsPlugin.scala
@@ -6,13 +6,22 @@ import sbt._
 
 object CtagsPlugin extends Plugin {
 
+  val ExternalSourcesDir = ".lib-src"
+
   val ctagsAdd = TaskKey[Unit]("ctagsAdd", "")
   val ctagsRemove = TaskKey[Unit]("ctagsRemove", "")
 
   val ctagsSettings = Seq[Setting[_]](
-    ctagsAdd <<= (thisProjectRef, buildStructure, state, allDependencies, defaultConfiguration) map {
-      (thisProjectRef, structure, state, deps, conf) ⇒
-        getSources(thisProjectRef, state, conf.get)
+    ctagsAdd <<= (thisProjectRef, buildStructure, state, allDependencies, defaultConfiguration, baseDirectory ) map {
+      (thisProjectRef, structure, state, deps, conf, base) ⇒
+        val sources = getSources(thisProjectRef, state, conf.get)
+        //sources foreach { a =>
+          //println (s"name: ${a._1.name}, file: $a._2")
+        //}
+        val toAdd = sources.filterKeys( _.name.contains("slf"))
+        toAdd foreach (source => unzipSource(sourceDir(base, source._1), source._1, source._2))
+
+        updateCtags
     }
   )
 
@@ -31,13 +40,24 @@ object CtagsPlugin extends Plugin {
       case _ ⇒ Map.empty
     }
   }
-  def evaluateTask[A](key: TaskKey[A], ref: ProjectRef, state: State): Option[(sbt.State, sbt.Result[A])] = {
-    EvaluateTask(Project.extract(state).structure, key, state, ref, EvaluateTask defaultConfig state)
+
+  def unzipSource(dest: File, moduleId: ModuleID, sourceJar: File): Unit = {
+    IO.delete(dest)
+    IO.createDirectory(dest)
+    IO.unzip(sourceJar, dest)
+  }
+
+  def sourceDir(baseDirectory: File, moduleId: ModuleID): File = {
+    val dir = moduleId.organization + "." + moduleId.name
+    new File(baseDirectory, ExternalSourcesDir + "/" + dir)
   }
 
   def updateCtags(): Unit = {
-    println("TODO - update")
+    "ctags" !
   }
 
+  def evaluateTask[A](key: TaskKey[A], ref: ProjectRef, state: State): Option[(sbt.State, sbt.Result[A])] = {
+    EvaluateTask(Project.extract(state).structure, key, state, ref, EvaluateTask defaultConfig state)
+  }
 }
 
